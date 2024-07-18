67dfeacc75651a4fce4ab11ccd190615147f171a
Nick Baumann <nickbaumann98@gmail.com>
2024-07-18 07:50:39 -0500

    Implement Netlify function for OpenAI API proxy

--- src/utils/gptService.js
+++ src/utils/gptService.js
@@ -5,7 +5,6 @@ export const getGPTResponse = async (prompt, day, objectives) => {
     console.log('Objectives:', objectives);
 
     try {
-        console.time('API Call Time');
         const response = await fetch('/.netlify/functions/openai-proxy', {
             method: 'POST',
             headers: {
@@ -13,18 +12,12 @@ export const getGPTResponse = async (prompt, day, objectives) => {
             },
             body: JSON.stringify({ prompt, day, objectives })
         });
-        console.timeEnd('API Call Time');
 
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
 
-        console.time('JSON Parse Time');
         const data = await response.json();
-        console.timeEnd('JSON Parse Time');
-
-        console.log('API Response:', data);
-
         return data.choices[0].message.content;
     } catch (error) {
         console.error('Error in getGPTResponse:', error);
